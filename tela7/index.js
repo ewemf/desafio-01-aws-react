2e8dfce01e846df5a77c86b6620a92b054f1e7ab
ewellyn <itsewellyn@gmail.com>
2024-09-05 19:03:11 -0300

    consertei um erro no js da tela 7

--- tela7/index.js
+++ tela7/index.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const userData = JSON.parse(localStorage.getItem('userData'));
+    const userData = JSON.parse(localStorage.getItem('infoUsuario'));
+    const censoredPassword = '•'.repeat((userData.password).length);
 
     if (userData) {
         document.getElementById('info-usuario').innerHTML = `
             <p>Name: ${userData.name}</p>
             <p>Email: ${userData.email}</p>
-            <p>Password: ••••••••••••••</p>
+            <p>Password: ${censoredPassword}</p>
         `;
     } else {
         document.getElementById('info-usuario').innerHTML = '<p>No user data found.</p>';
